996c31ba67b8558a6eccb2c4b2df37fd2ff6b3f0
AleWu <thealewu@gmail.com>
2023-04-14 01:50:13 +0200

    First real running version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', loadChildren: () => import('./pages/index/index.module').then(m => m.IndexModule) }
+  { path: '', pathMatch: 'full', loadChildren: () => import('./pages/index/index.module').then(m => m.IndexModule) },
+  { path: 'linalg', loadChildren: () => import('./pages/linalg/linalg.module').then(m => m.LinalgModule) }
 ];
 
 @NgModule({
